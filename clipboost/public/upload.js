9b9b1deed0265527bc24006d4be84d570aeba231
JakeOfAllTime <clipboost.ai@gmail.com>
2025-08-12 23:43:26 -0700

    Removed clipboost folder, cleaned root

--- clipboost/public/upload.js
+++
@@ -1,9 +0,0 @@
-async function uploadClips() {
-  const files = document.getElementById('clipUpload').files;
-  const storage = firebase.storage();
-  for (let file of files) {
-    const ref = storage.ref(`clips/${file.name}`);
-    await ref.put(file);
-    console.log(`${file.name} uploaded!`);
-  }
-}
